5562a2e259aea22434a1a731e7d57a0046ac461a
igoshinaleksandr <igoshin.aleksandr@gmail.com>
2025-11-02 20:08:36 +0400

    Version 1.1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from sys import prefix
 
 from aiogram import  Bot, Dispatcher, types
-from  aiogram.filters.command import Command
+from  aiogram.filters.command import Command, CommandObject
 from api_token import TOKEN
 
 bot = Bot(TOKEN)
@@ -13,19 +13,21 @@ TEXT = '''
 /start - запуск бота
 '''
 
-@dp.message(Command('start')
+@dp.message(Command('start'))
 async def start_command(message: types.Message):
     await message.answer('Привет,я бот!')
 
 
-@dp.message(Command('help', prefix='$'))
+@dp.message(Command('help'))
 async def help_command(message: types.Message):
     await message.reply(TEXT)
 
-
+@dp.message(Command('test_args'))
+async def test_args_command(message: types.Message, command: CommandObject):
+    await message.answer(command.args)
 
 async def main():
-    await bot.delete.webhook(drop_pending_updates=True) #проигнорировать апдейты бота
+    await bot.delete_webhook(drop_pending_updates=True) #проигнорировать апдейты бота
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
